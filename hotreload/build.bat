50d6282fa6123b1579df0dae565bd2874fb96407
Luminight <balazscsabai98@gmail.com>
2025-01-10 23:18:18 +0100

    add gitignore, fix core.o path

--- hotreload/build.bat
+++ hotreload/build.bat
@@ -2,5 +2,5 @@ set LDFLAGS=-lgdi32 -lwinmm -Llibs -lraylib -lpthread -lm -L./bin
 
 mkdir bin
 
-gcc -c -o core.o core.c
+gcc -c -o ./build/core.o core.c
 gcc -o ./bin/core.dll -shared core.c %LDFLAGS%
