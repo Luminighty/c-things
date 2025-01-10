4dd1edad79e5513fbf75103f89f3647d884480a2
Luminight <balazscsabai98@gmail.com>
2025-01-10 23:24:52 +0100

    update build.bat

--- hotreload/build.bat
+++ hotreload/build.bat
@@ -3,4 +3,4 @@ set LDFLAGS=-lgdi32 -lwinmm -Llibs -lraylib -lpthread -lm -L./bin
 mkdir bin
 
 gcc -c -o ./build/core.o core.c
-gcc -o ./bin/core.dll -shared core.c %LDFLAGS%
+gcc -o ./bin/core.dll -shared ./build/core.o %LDFLAGS%
